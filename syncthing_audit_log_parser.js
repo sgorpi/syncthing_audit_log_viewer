02d0f7d0ce3884ecde5dfcfea3abd02f64512664
Sgorpi <sgorpi@gmail.com>
2025-10-28 07:08:32 +0100

    Added filter/search for label and path

--- syncthing_audit_log_parser.js
+++ syncthing_audit_log_parser.js
@@ -6,6 +6,8 @@
  * You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
+var typingTimer;
+
 function add_table_cell_with_time(row, time) {
     const cell = row.insertCell(0)
     cell.className = "nowrap";
@@ -17,6 +19,7 @@ function add_table_cell_with_time(row, time) {
 
 
 var device_map = {}
+var folders = []
 function fill_id_to_device_name_map(log_object_map) {
     Object.entries(log_object_map).forEach(([, jsonData]) => {
         var short_id = null
@@ -28,9 +31,23 @@ function fill_id_to_device_name_map(log_object_map) {
             short_id = jsonData["data"]["myID"].split("-")[0]
             device_map[short_id] = "me";
             device_map[jsonData["data"]["myID"]] = "me";
+        } else if (jsonData["type"].includes("ChangeDetected")) {
+            if (!folders.includes(jsonData["data"]["label"])) {
+                folders.push(jsonData["data"]["label"])
+            }
         }
     })
     console.log(device_map)
+    // update label select
+    const label_select = $("#label_select")
+    label_select.empty()
+    label_select.append($("<option>", { value: "", text: "- no label -" }))
+    $.each(folders, function (idx, val) {
+        label_select.append($("<option>", { value: val, text: val }))
+        console.log("label: " + val)
+    })
+    $("#label_select").data("plugin_multiSelect").updateMenuItems()
+
     return device_map;
 }
 
@@ -112,9 +129,9 @@ function add_table_row_startup_complete(tableBody, time, jsonData) {
     cell.innerHTML = jsonData["data"]["myID"]
 }
 
+
 var log_object_map = {};
 function display() {
-    const tableBody = document.querySelector("#dataTable tbody");
     const type_map = {
         "LocalChangeDetected": add_table_row_change_detected,
         "RemoteChangeDetected": add_table_row_change_detected,
@@ -122,20 +139,45 @@ function display() {
         "DeviceDisconnected": add_table_row_device_connection,
         "StartupComplete": add_table_row_startup_complete,
     }
+    const tableBody = document.querySelector("#dataTable tbody");
+    const path_search = $("#path_search").val()
+    const label_selected = $("#label_select").val()
+    console.log("Label selected: " + label_selected)
+
+    function matches_user_filter(jsonData) {
+        var ret = true
+        if (label_selected.length > 0) {
+            if ("label" in jsonData["data"]) {
+                ret &= label_selected.includes(jsonData["data"]["label"])
+            } else {
+                ret = label_selected.includes("");
+            }
+        }
+        if ("path" in jsonData["data"]) {
+            ret &= (jsonData["data"]["path"].toLowerCase().includes(path_search));
+        } else if ("id" in jsonData["data"]) {
+            ret &= (jsonData["data"]["id"].toLowerCase().includes(path_search));
+        } else if ("myID" in jsonData["data"]) {
+            ret &= (jsonData["data"]["myID"].toLowerCase().includes(path_search));
+        }
+        return ret;
+    }
+
+    tableBody.innerHTML = "";
     Object.keys(log_object_map).sort().forEach(time => {
         const jsonData = log_object_map[time];
         //console.log("Parsing: " + time + " = " + JSON.stringify(jsonData, null, 2))
-        if (jsonData["type"] in type_map) {
+        if (jsonData["type"] in type_map && matches_user_filter(jsonData)) {
             type_map[jsonData["type"]](tableBody, time, jsonData)
         } else {
-            console.log("Ignored " + jsonData["type"])
+            //console.log("Ignored " + jsonData["type"])
         }
     }
     )
 }
 
 function parse(auditlog_txt, file) {
-    const resultDiv = document.getElementById("result");
+    const resultDiv = $("#result");
     try {
         const lines = auditlog_txt.split("\n")
         for (var line = 0; line < lines.length; line++) {
@@ -147,28 +189,26 @@ function parse(auditlog_txt, file) {
                 log_object_map[time] = jsonData;
 
             } catch (err) {
-                resultDiv.innerHTML += "Invalid JSON line " + line + ": '" + lines[line] + " -- " + err + "'<br/>";
+                resultDiv.append("Invalid JSON line " + line + ": '" + lines[line] + " -- " + err + "'<br/>");
             }
         }
-        resultDiv.innerHTML += "Loaded " + lines.length + " lines of log from " + file.name + "<br />";
-
-        fill_id_to_device_name_map(log_object_map);
+        resultDiv.append("Loaded " + lines.length + " lines of log from " + file.name + "<br />");
     } catch (err) {
         resultDiv.innerHTML += "Error parsing: " + err + "<br />";
     }
 };
 
-window.onload = function () {
-    document.getElementById("parseBtn").addEventListener("click", function () {
-        const fileInput = document.getElementById("fileInput");
-        const resultDiv = document.getElementById("result");
-        const tableBody = document.querySelector("#dataTable tbody");
 
-        resultDiv.innerHTML = "";
-        tableBody.innerHTML = "";
+$(document).ready(function () {
+    //alert('loaded')
+    $("#parseBtn").on("click", function () {
+        const fileInput = document.getElementById("fileInput");
+        const resultDiv = $("#result");
+
+        resultDiv.html("");
 
         if (fileInput.files.length === 0) {
-            resultDiv.innerHTML = "Please select a JSON file first.";
+            resultDiv.append("Please select a JSON file first.");
             return;
         }
         var num_loaded = 0
@@ -179,10 +219,26 @@ window.onload = function () {
                 parse(e.target.result, file)
                 num_loaded++;
                 if (num_loaded === fileInput.files.length) {
+                    fill_id_to_device_name_map(log_object_map);
                     display()
                 }
             };
             reader.readAsText(file);
         }
     });
-}
+    $("#path_search").on("keyup", function () {
+        clearTimeout(typingTimer)
+        typingTimer = setTimeout(display, 300)
+    })
+    $("#path_search").on("keydown", function () {
+        clearTimeout(typingTimer)
+    })
+    $("#label_select").multiSelect({
+        "allText": "-- everything --",
+        "noneText": "-- everything --",
+    })
+    $("#label_select").on("change", function () {
+        display()
+    })
+
+});
